2f270608901438b693bed3a41a3e06267ccc81c8
AmazingWei <wei5i42@qq.com>
2020-05-28 09:32:28 +0800

    update files: 20200528 09:32

--- diy.sh
+++ diy.sh
@@ -5,6 +5,10 @@ echo 'Update luci-theme-argon'
 rm -rf /home/runner/openwrt/package/lean/luci-theme-argon/
 cp -R /home/runner/luci-theme-argon/ /home/runner/openwrt/package/lean/
 
+echo 'Update luci-app-ssr-plus'
+rm -rf /home/runner/openwrt/package/lean/luci-app-ssr-plus/
+cp -R /home/runner/helloworld/luci-app-ssr-plus/ /home/runner/openwrt/package/lean/
+
 echo 'Fix serverchan temperature bug'
 sed -i "s?/sys/class/thermal/thermal_zone\*/temp?/sys/class/hwmon/hwmon0/temp1_input?g" /home/runner/openwrt/package/ctcgfw/luci-app-serverchan/root/usr/bin/serverchan/serverchan
 
